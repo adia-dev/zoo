ba724a8e9ec7efb42def23e6c90f3bbad6d05342
paschyz <paschy2002@gmail.com>
2023-05-30 20:47:38 +0200

    added eroor handling of token auth

--- src/middlewares/user.middleware.ts
+++ src/middlewares/user.middleware.ts
@@ -1,38 +1,49 @@
-import {Request, RequestHandler} from "express";
-import { IUser, ISession, SessionModel} from "../models";
+import { Request, RequestHandler } from 'express';
+import { IUser, ISession, SessionModel } from '../models';
 
 declare module 'express' {
-    export interface Request {
-        user?: IUser;
-    }
+  export interface Request {
+    user?: IUser;
+  }
 }
 
 export function checkUserToken(): RequestHandler {
-    return async function(req: Request, res, next) {
-        const authorization = req.headers['authorization'];
-        if(authorization === undefined) {
-            res.status(401).end(); // unauthorized
-            return;
-        }
-        const parts = authorization.split(' ');
-        if(parts.length !== 2 || parts[0] !== 'Bearer') {
-            res.status(401).end(); // unauthorized
-            return;
-        }
-        const token = parts[1];
-        console.log(token);
-        // populate permet de faire une jointure sur la collection qui est deriere le champs user
-        const session = await SessionModel.findById(token).populate({
-            path: "user",
-            populate: {
-                path: "roles"
-            }
-        }).exec();
-        if(session === null) {
-            res.status(401).end(); // unauthorized
-            return;
-        }
-        req.user = session.user as IUser;
-        next();
-    };
+  return async function (req: Request, res, next) {
+    try {
+      const authorization = req.headers['authorization'];
+      if (authorization === undefined) {
+        res.status(401).end(); // unauthorized
+        return;
+      }
+
+      const parts = authorization.split(' ');
+      if (parts.length !== 2 || parts[0] !== 'Bearer') {
+        res.status(401).end(); // unauthorized
+        return;
+      }
+
+      const token = parts[1];
+      console.log(token);
+
+      // populate permet de faire une jointure sur la collection qui est deriere le champs user
+      const session = await SessionModel.findById(token)
+        .populate({
+          path: 'user',
+          populate: {
+            path: 'role',
+          },
+        })
+        .exec();
+
+      if (session === null) {
+        res.status(401).end(); // unauthorized
+        return;
+      }
+
+      req.user = session.user as IUser;
+      next();
+    } catch (error) {
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  };
 }
